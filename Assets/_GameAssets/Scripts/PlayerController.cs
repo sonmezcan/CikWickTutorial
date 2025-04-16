0059d190bb1d2c3c38119703f092f98ae24719fb
Uraz Sönmez <sonmezcan1997@gmail.com>
2025-04-16 22:56:52 +0300

    char controller

--- Assets/_GameAssets/Scripts/PlayerController.cs
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 
     [Header("Movement Settings")]
     [SerializeField] private float _movementSpeed;
+    [SerializeField] private KeyCode _movementKey;
 
     [Header("Jump Settings")]
 
@@ -16,15 +17,27 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _jumpCooldown;
     [SerializeField] private bool _canJump;
 
+    [Header("Slider Settings")]
+
+    [SerializeField] private KeyCode _slideKey;
+
+    [SerializeField] private float _slideMultiplier;
+
+    [SerializeField] private float _slideDrag;
+
     [Header("Ground Check Settings")]
 
     [SerializeField] private float _playerHeight;
     [SerializeField] private LayerMask _groundLayer;
 
+    [SerializeField] private float _groundDrag;
+
     private Rigidbody _playerRigidbody;
     
     private float _horizontalInput, _verticalInput;
     private Vector3 _movementDirection;
+
+    private bool _isSliding;
     private void Awake()
     {
         _playerRigidbody = GetComponent<Rigidbody>();
@@ -33,6 +46,8 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         SetInputs();
+        SetPlayerDrag();
+        LimitPlayerSpeed();
     }
     private void FixedUpdate()
     {
@@ -44,7 +59,16 @@ public class PlayerController : MonoBehaviour
        _horizontalInput = Input.GetAxisRaw("Horizontal");
        _verticalInput = Input.GetAxisRaw("Vertical"); 
 
-       if(Input.GetKey(_jumpKey) && _canJump && IsGrounded())
+       if(Input.GetKeyDown(_slideKey))
+       {
+            _isSliding = true;
+       }
+       else if (Input.GetKeyDown(_movementKey))
+       {
+            _isSliding = false;
+       }
+
+       else if(Input.GetKey(_jumpKey) && _canJump && IsGrounded())
        {
             _canJump = false;
             SetPlayerJumping();
@@ -56,8 +80,40 @@ public class PlayerController : MonoBehaviour
     {
         _movementDirection = _orientationTransform.forward * _verticalInput 
         + _orientationTransform.right * _horizontalInput;
+        if(_isSliding)
+        {
+            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed * _slideMultiplier, ForceMode.Force);
+        }
+        else
+        {
+            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
+        }
+        
+    }
 
-        _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
+    private void SetPlayerDrag()
+    {   
+        if(_isSliding)
+        {
+             _playerRigidbody.linearDamping = _slideDrag;
+        }
+        else 
+        {
+            _playerRigidbody.linearDamping = _groundDrag;
+        }
+    }
+
+    private void LimitPlayerSpeed()
+    {
+        Vector3 flatVelocity = new Vector3(_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);
+
+        if(flatVelocity.magnitude > _movementSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * _movementSpeed;
+            _playerRigidbody.linearVelocity = new Vector3(limitedVelocity.x, _playerRigidbody.linearVelocity.y, limitedVelocity.z);
+
+        }
+
     }
 
     private void SetPlayerJumping()
